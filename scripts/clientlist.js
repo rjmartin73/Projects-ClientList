b2da763327e8ed01400730390d2838afdecd1caa
Ryan Martin <ryan.martin@clearviewlive.com>
2020-07-17 09:46:21 -0600

    Updated client list

--- scripts/clientlist.js
+++ scripts/clientlist.js
@@ -2,7 +2,7 @@ const formLoad = () => {
 	const searchBtn = document.getElementById('submit');
 	const searchForm = document.getElementsByTagName('form');
 	const searchBox = document.getElementById('searchbox');
-	const listUpdatedDate = '7/9/2020';
+	const listUpdatedDate = '7/17/2020';
 
 	document.querySelector('#date-updated').innerHTML = "Client list as of <strong>" + listUpdatedDate.toString() + "</strong>" 
 
